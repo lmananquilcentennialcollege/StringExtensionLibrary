f0aba7f41b8e0b999b85429bb1d8cc3dc7aa7654
Leandro Mananquil <lmananqu@my.centennialcollege.ca>
2025-03-27 13:37:29 -0400

    Add project files.

---
+++ StringExtensionLibrary/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StringExtensionLibrary
+{
+    public static class Class1
+    {
+        public static bool StartsWithUpperCase(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str)) return false;
+            return char.IsUpper(str[0]);
+        }
+
+    }
+}
